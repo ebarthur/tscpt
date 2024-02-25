e5ba7d813d5008dba21d684c130857e4c1c5ee4d
Ebenezer Arthur <arthurebenezer@aol.com>
2024-02-25 14:36:35 +0000

    type assertion & generics

--- build/js/main.js
+++ build/js/main.js
@@ -196,13 +196,13 @@ class Coder {
         this.isEmployed = isEmployed;
     }
     greet() {
-        return `Hello, my name is ${this.name} and I am ${this.isEmployed ? 'employed' : 'unemployed'}`;
+        return `Hello, my name is ${this.name} and I am ${this.isEmployed ? "employed" : "unemployed"}`;
     }
     getAge() {
         return `Age: ${this.age}`;
     }
 }
-const person = new Coder('Cramuel', 20, ['Golang', 'C++'], false);
+const person = new Coder("Cramuel", 20, ["Golang", "C++"], false);
 console.log(person.greet());
 console.log(person.getAge());
 class WebDev extends Coder {
@@ -210,6 +210,111 @@ class WebDev extends Coder {
         super(name, age, lang, isEmployed);
     }
     greet() {
-        return `Hello, my name is ${this.name} and I am a web developer`;
+        return `Hello, my name is ${this.name} and I am a web developer.`;
     }
 }
+const prog = new Coder("Arthur", 18, ["Java"], true);
+console.log(prog.greet());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const me = new Guitarist("Van", "Bass Guitar");
+console.log(me.play("strikes"));
+// ---------------------------------
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps("John");
+const MIke = new Peeps("Mike");
+const Andy = new Peeps("Andy");
+console.log(Peeps.count);
+console.log(John.id);
+// ---------------------------------
+// Getters and Setters
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error("Param is not an array of strings");
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ["Neil Young", "Led Zep"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "ZZ Top0"];
+console.log(MyBands.data);
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+// Dynamic access
+let prop = "Pizza";
+console.log(todaysTransactions[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransactions));
+const student = {
+    firstName: "Amy",
+    GPA: 3.04,
+    test: 32,
+};
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, "GPA");
+const monthlyIncomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250,
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
+/*
+ Generics
+ 25-02-2024
+ */
+const stringEcho = (arg) => arg;
+console.log(stringEcho("Mano"));
+const echo = (arg) => arg; // makinng it generic so it can take arguments of any type
+const isObj = (args) => {
+    return typeof args === "object" && !Array.isArray(args) && args !== null;
+};
+console.log(isObj(true));
+console.log(isObj("John"));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: "John" }));
--- src/main.ts
+++ src/main.ts
@@ -282,10 +282,9 @@ console.log(addOrConcat(4, 5, "concat")); // 45
 console.log(addOrConcat(4, 5, "add")); // 9
 
 // The DOM
-const img = document.getElementById("#img")! // non-null assertion operator
+const img = document.getElementById("#img")!; // non-null assertion operator
 const myinput = document.getElementById("input") as HTMLInputElement;
 const myimg = <HTMLImageElement>document.getElementById("img"); // this will not work in tsx files or react
-
 
 /*
  Classes
@@ -298,7 +297,12 @@ class Coder {
   protected lang: string[];
   private readonly isEmployed: boolean;
 
-  constructor(name: string, age: number, lang: string[] = [], isEmployed: boolean) {
+  constructor(
+    name: string,
+    age: number,
+    lang: string[] = [],
+    isEmployed: boolean
+  ) {
     this.name = name;
     this.age = age;
     this.lang = lang;
@@ -306,15 +310,15 @@ class Coder {
   }
 
   public greet() {
-    return `Hello, my name is ${this.name} and I am ${this.isEmployed ? 'employed' : 'unemployed'}`;
+    return `Hello, my name is ${this.name} and I am ${this.isEmployed ? "employed" : "unemployed"}`;
   }
 
   getAge() {
-    return `Age: ${this.age}`
+    return `Age: ${this.age}`;
   }
 }
 
-const person = new Coder('Cramuel', 20, ['Golang', 'C++'], false);
+const person = new Coder("Cramuel", 20, ["Golang", "C++"], false);
 console.log(person.greet());
 console.log(person.getAge());
 
@@ -324,6 +328,194 @@ class WebDev extends Coder {
   }
 
   public greet() {
-    return `Hello, my name is ${this.name} and I am a web developer`;
+    return `Hello, my name is ${this.name} and I am a web developer.`;
   }
 }
+
+const prog = new Coder("Arthur", 18, ["Java"], true);
+
+console.log(prog.greet());
+
+// Implementing an interface to a class
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+
+  public play(action: string): string {
+    return `${this.name} ${action} the ${this.instrument}`;
+  }
+}
+
+const me = new Guitarist("Van", "Bass Guitar");
+
+console.log(me.play("strikes"));
+
+// ---------------------------------
+
+class Peeps {
+  static count: number = 0;
+
+  static getCount(): number {
+    return Peeps.count;
+  }
+
+  public id: number;
+
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
+
+const John = new Peeps("John");
+const MIke = new Peeps("Mike");
+const Andy = new Peeps("Andy");
+
+console.log(Peeps.count);
+console.log(John.id);
+
+// ---------------------------------
+
+// Getters and Setters
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    } else throw new Error("Param is not an array of strings");
+  }
+}
+
+const MyBands = new Bands();
+MyBands.data = ["Neil Young", "Led Zep"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "ZZ Top0"];
+console.log(MyBands.data);
+
+/*
+ Index Signatures & keyof Assertions
+ 25-02-2024
+ */
+
+// Index Signatures
+
+// interface TransactionObj {
+//   [index: string]: number;
+// }
+
+interface TransactionObj {
+  [index: string]: number;
+  Pizza: number;
+  Books: number;
+  Job: number;
+}
+
+const todaysTransactions: TransactionObj = {
+  Pizza: -10,
+  Books: -5,
+  Job: 50,
+};
+
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+
+// Dynamic access
+let prop: string = "Pizza";
+console.log(todaysTransactions[prop]);
+
+const todaysNet = (transactions: TransactionObj): number => {
+  let total = 0;
+  for (const transaction in transactions) {
+    total += transactions[transaction];
+  }
+  return total;
+};
+
+console.log(todaysNet(todaysTransactions));
+
+// ---------------------------------
+
+interface Student {
+  [index: string]: string | number | string[] | undefined;
+  firstName: string;
+  GPA: number;
+  classes?: string[];
+}
+
+const student: Student = {
+  firstName: "Amy",
+  GPA: 3.04,
+  test: 32,
+};
+
+for (const key in student) {
+  console.log(`${key}: ${student[key as keyof Student]}`);
+}
+
+const logStudentKey = (student: Student, key: keyof Student): void => {
+  console.log(`Student ${key}: ${student[key]}`);
+};
+
+logStudentKey(student, "GPA");
+
+// ---------------------------------
+
+// interface Incomes{
+//   [key: string]: number
+// }
+
+type Streams = "salary" | "bonus" | "sidehustle";
+
+type Incomes = Record<Streams, number>;
+
+const monthlyIncomes: Incomes = {
+  salary: 500,
+  bonus: 100,
+  sidehustle: 250,
+};
+
+for (const revenue in monthlyIncomes) {
+  console.log(monthlyIncomes[revenue as keyof Incomes]);
+}
+
+/* 
+ Generics
+ 25-02-2024
+ */
+
+const stringEcho = (arg: string): string => arg;
+
+console.log(stringEcho("Mano"));
+
+const echo = <T>(arg: T): T => arg; // makinng it generic so it can take arguments of any type
+
+const isObj = <T>(args: T): boolean => {
+  return typeof args === "object" && !Array.isArray(args) && args !== null;
+};
+
+console.log(isObj(true));
+console.log(isObj("John"));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: "John" }));
